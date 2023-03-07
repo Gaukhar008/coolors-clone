fbce5753e9debfc3667b2782619b19755c33ddc2
Gaukhar Abdizhapbarova <gauhar.abdizhapbarova@gmail.com>
2023-03-07 20:04:11 +0600

    Add functions to copy color, update location hash, get colors from hash

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ document.addEventListener('click', event => {
 
         node.classList.toggle('fa-lock-open');
         node.classList.toggle('fa-lock');
+    } else if (type === 'copy') {
+        copyToClickboard(event.target.textContent);
     }
 
 })
@@ -29,17 +31,42 @@ function generateRandomColor() {
     return '#' + color;
 }
 
-function setRandomColors() {
-    cols.forEach(col => {
+function copyToClickboard(text) {
+    return navigator.clipboard.writeText(text);
+}
+
+function setRandomColors(isInitial) {
+    const colors = isInitial ? getColorsFromHash() : [];
+
+    cols.forEach((col, index) => {
+        const isLocked = col.querySelector('i').classList.contains('fa-lock');
         const text = col.querySelector('h2');
         const button = col.querySelector('button');
-        const color = generateRandomColor();
+
+        if (isLocked) {
+            colors.push(text.textContent);
+            return;
+        }
+
+        const color = isInitial
+            ? colors[index]
+                ? colors[index] : generateRandomColor()
+            : generateRandomColor();
+
+        if (!isInitial) {
+            colors.push(color);
+        }
+
+        colors.push(color);
+
         text.textContent = color;
         col.style.background = color;
 
         setTextColor(text, color);
         setTextColor(button, color);
-    })
+    });
+
+    updateColorsHash(colors);
 }
 
 function setTextColor(text, color) {
@@ -47,5 +74,19 @@ function setTextColor(text, color) {
     text.style.color = luminance > 0.5 ? 'black' : 'white';
 }
 
-setRandomColors();
+function updateColorsHash(colors = []) {
+    document.location.hash = colors.map(col => col.toString().substring(1)).join('-');
+}
+
+function getColorsFromHash() {
+    if (document.location.hash.length > 1) {
+        return document.location.hash
+            .substring(1)
+            .split('-')
+            .map(color => '#' + color);
+    }
+    return [];
+}
+
+setRandomColors(true);
 
